7966754a4882efa8dbc87a847e01026f0e2228e2
Ajwad Mahi <mahi.kabir@yahoo.com>
2021-03-27 02:06:53 +0600

    burj-al-arab

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBYLXOgfqQPE8M_pyK6Ti9BLwhedxwcED8",
+  authDomain: "burj-al-arab-master-a50a1.firebaseapp.com",
+  projectId: "burj-al-arab-master-a50a1",
+  storageBucket: "burj-al-arab-master-a50a1.appspot.com",
+  messagingSenderId: "250649788534",
+  appId: "1:250649788534:web:4c706516b10b1e4bab1f57"
+  };
+  export default firebaseConfig;
